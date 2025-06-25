ce0c2525fc6d6a00c3d8b7240359bb1531bab2da
grazielle-bastos <grazielle1101@gmail.com>
2025-06-25 12:12:31 -0300

    Commit inicial

---
+++ src/Ex2Main.java
@@ -0,0 +1,11 @@
+public class Ex2Main {
+    public static void main(String[] args) {
+//● Crie um programa que receba uma lista de números inteiros do usuário e use streams para:
+//1. Filtrar os números ímpares.
+//2. Dobrar os valores dos números ímpares.
+//3. Ordenar os números resultados em ordem crescente.
+//4. Exibir a lista final.
+
+
+    }
+}
---
+++ src/Ex3Main.java
@@ -0,0 +1,12 @@
+public class Ex3Main {
+    public static void main(String[] args) {
+//● Crie um programa que armazene uma lista de pessoas (nome e idade) usando um Map.
+//1. Armazene as informações em um Map<String, Integer>, onde a chave é o nome e o
+//valor é a idade.
+//2. Use um Set para armazenar os nomes das pessoas com mais de 30 anos.
+//3. Exiba os nomes das pessoas que possuem mais de 30 anos, ordenados em ordem alfabética.
+
+
+
+    }
+}
