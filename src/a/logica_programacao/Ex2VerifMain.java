0602f7a4d33303feddb2735a07c88ab4feaf7cf4
grazielle-bastos <grazielle1101@gmail.com>
2025-06-26 08:32:55 -0300

    Exercício par/ímpar

--- src/a/logica_programacao/Ex2VerifMain.java
+++ src/a/logica_programacao/Ex2VerifMain.java
@@ -1,9 +1,22 @@
 package a.logica_programacao;
 
+import java.util.Scanner;
+
 public class Ex2VerifMain {
     public static void main(String[] args) {
         //Verificação de Número Par ou Ímpar:
         //Crie um programa que verifique se um número é par ou ímpar.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Este número é par");
+        } else {
+            System.out.println("Este número é ímpar");
+        }
 
     }
 }
